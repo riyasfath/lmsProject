a29c5796b7702bd293551d6148dc74d2957b0ec2
riyasfath <riyasfath@gmail.com>
2022-12-21 12:34:12 +0530

    messga

---
+++ Desktop/NormalList-master/app/src/main/java/com/example/normallistd/Swaraj.java
@@ -0,0 +1,14 @@
+package com.example.normallistd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Swaraj extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swaraj);
+    }
+}
